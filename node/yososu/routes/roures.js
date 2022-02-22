e813be6fa7c867368577fa0b9ff1a93927ffe233
sensato437 <64115708+sensato437@users.noreply.github.com>
2022-02-22 09:05:12 +0900

    Update roures.js

--- node/yososu/routes/roures.js
+++ node/yososu/routes/roures.js
@@ -17,75 +17,14 @@ var User = mongoose.model('yososu',userSchema);
 app.get('/list', function(req, res, next) {
       User.find({},function(err,docs){
         if(err) console.log('err');
-               res.writeHead(200);
-               var template = `
-               <!doctype html>
-               <html>
-               <head>
-                 <title>Result</title>
-                 <meta charset="utf-8">
-               </head>
-               <body>
-                <table border="1" margin: auto; text-align: center;>
-                  <tr>
-                    <th> 이름 </th>
-                    <th> 주소 </th>
-                    <th> 가격 </th>
-                    <th> 잔량(L) </th>
-                  </th>`;
-                  for(var i=0;i<docs.length;i++){
-                    template += `
-                  <tr>
-                    <th>${docs[i]['name']}</th>
-                    <th>${docs[i]['addr']}</th>
-                    <th>${docs[i]['price']}</th>
-                    <th>${docs[i]['inventory']}</th>
-                  </tr>`
-                }
-                 template +=`</table>
-               </body>
-               </html>
-              `;
-               res.end(template);
+            res.send(docs);
       });
 });
 app.get("/api/finds",(req,res) => {
   const addr = req.query.addr;
   User.find({'addr' :{$regex:addr}}).select('addr name price inventory').exec(function(err,docs){
     if(err) console.log('err');
-           res.writeHead(200);
-           var template = `
-           <!doctype html>
-           <html>
-           <head>
-             <title>Result</title>
-             <meta charset="utf-8">
-           </head>
-           <body>
-            <table border="1" margin: auto; text-align: center;>
-              <tr>
-                <th> 이름 </th>
-                <th> 주소 </th>
-                <th> 가격 </th>
-                <th> 잔량(L) </th>
-              </th>
-              `;
-              for(var i=0;i<docs.length;i++){
-                template += `
-              <tr>
-                <th>${docs[i]['name']}</th>
-                <th>${docs[i]['addr']}</th>
-                <th>${docs[i]['price']}</th>
-                <th>${docs[i]['inventory']}</th>
-              </tr>
-              `;
-            }
-             template +=`
-            </table>
-           </body>
-           </html>
-          `;
-           res.end(template);
+          res.send(docs);
   })
 });
 
@@ -93,39 +32,7 @@ app.post("/api/find/addr",(req,res) => {
   const addr = req.body.addr;
   User.find({'addr' :{$regex:addr}}).select('addr name price inventory').exec(function(err,docs){
     if(err) console.log('err');
-           res.writeHead(200);
-           var template = `
-           <!doctype html>
-           <html>
-           <head>
-             <title>Result</title>
-             <meta charset="utf-8">
-           </head>
-           <body>
-            <table border="1" margin: auto; text-align: center;>
-              <tr>
-                <th> 이름 </th>
-                <th> 주소 </th>
-                <th> 가격 </th>
-                <th> 잔량(L) </th>
-              </th>
-              `;
-              for(var i=0;i<docs.length;i++){
-                template += `
-              <tr>
-                <th>${docs[i]['name']}</th>
-                <th>${docs[i]['addr']}</th>
-                <th>${docs[i]['price']}</th>
-                <th>${docs[i]['inventory']}</th>
-              </tr>
-              `;
-            }
-             template +=`
-            </table>
-           </body>
-           </html>
-          `;
-           res.end(template);
+            res.send(docs);
   })
 });
 
